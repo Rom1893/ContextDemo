7bafb4d21c7bdf7630bb570fb65f37c8c1559453
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-14 12:25:28 -0300

    PageContento to functional component

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,19 +1,16 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-export default function PageContent() {
+export default function PageContent(props) {
 
-    static contextType = ThemeContext;
-
-
-  const { isDarkMode } = this.context;
+  const { isDarkMode } = useContext(ThemeContext)
   const styles = {
     backgroundColor: isDarkMode ? "black" : "white",
     height: "100vh",
     width: "100vw"
   }
   return (
-    <div style={styles}>{this.props.children}</div>
+    <div style={styles}>{props.children}</div>
   )
 
 }
