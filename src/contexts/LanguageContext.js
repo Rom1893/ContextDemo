84ec6dc02b0465f3f8fe28a6e6a9d532b0fb2ce4
Rodrigo Morán <rodrigobq18@gmail.com>
2022-11-14 12:12:56 -0300

    LanguageContext to functional component

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,24 +1,19 @@
-import React, { Component, createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
+export function LanguageProvider(props) {
 
-    state = {
-        language: "english"
-    }
-
-    changeLanguage = (e) => {
-        this.setState({
-            language: e.target.value
-        })
-    }
-
-  render() {
-    return (
-      <LanguageContext.Provider value={{...this.state, changeLanguage: this.changeLanguage}}>
-        {this.props.children}
-      </LanguageContext.Provider>
-    )
+  const [language, setLanguage] = useState("english")
+  const changeLanguage = (e) => {
+    setLanguage(e.target.value)
   }
+
+
+  return (
+    <LanguageContext.Provider value={{language, changeLanguage}}>
+      {props.children}
+    </LanguageContext.Provider>
+  )
+
 }
